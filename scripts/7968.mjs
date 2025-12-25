8a776e6d3f3fbc9fd0654373108d892979316bdb
F88 <685250+F88@users.noreply.github.com>
2025-12-25 14:15:02 +0900

    fix(script): correct formatting and add missing commas in prototype script

--- scripts/7968.mjs
+++ scripts/7968.mjs
@@ -4,17 +4,17 @@ import { createPromidasForLocal } from '@f88/promidas';
 async function main() {
   const repo = createPromidasForLocal({
     protopediaApiToken: process.env.PROTOPEDIA_API_V2_TOKEN,
-    logLevel: 'debug '
+    logLevel: 'debug ',
   });
 
   const result = await repo.setupSnapshot({
     // offset: 5500,
     // limit: 1000,
-    prototypeId: 7968
+    prototypeId: 7968,
   });
 
-  console.log(await repo.analyzePrototypes())
-  const p = await repo.getPrototypeFromSnapshotByPrototypeId(7968)
+  console.log(await repo.analyzePrototypes());
+  const p = await repo.getPrototypeFromSnapshotByPrototypeId(7968);
   console.dir(p, { depth: null });
 }
 
