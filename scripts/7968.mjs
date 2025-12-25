087f3d969ec57df5eaff689d19fbdd8f4f299a6a
F88 <685250+F88@users.noreply.github.com>
2025-12-25 14:06:08 +0900

    feat(script): add initial script for prototype analysis and snapshot setup

---
+++ scripts/7968.mjs
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { createPromidasForLocal } from '@f88/promidas';
+
+async function main() {
+  const repo = createPromidasForLocal({
+    protopediaApiToken: process.env.PROTOPEDIA_API_V2_TOKEN,
+    logLevel: 'debug '
+  });
+
+  const result = await repo.setupSnapshot({
+    // offset: 5500,
+    // limit: 1000,
+    prototypeId: 7968
+  });
+
+  console.log(await repo.analyzePrototypes())
+  const p = await repo.getPrototypeFromSnapshotByPrototypeId(7968)
+  console.dir(p, { depth: null });
+}
+
+main();
